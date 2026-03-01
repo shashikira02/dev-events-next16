37daa28a5d71aa52156d5323639a6a36a3cf1b2b
Shashi Kiran Perka <shashikira4124@gmail.com>
2026-03-02 05:13:33 +0530

    fixing vercel bugs

--- src/app/page.js
+++ src/app/page.js
@@ -19,7 +19,7 @@ const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 const Page = async () => {
   'use cache'
   cacheLife('hours');
-  const response = await fetch(`${BASE_URL}/api/events`);
+  const response = await fetch(`${BASE_URL}/api/events`, { cache: 'no-store' });
   const { events } = await response.json();
 
   return (
